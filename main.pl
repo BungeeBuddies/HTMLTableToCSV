ac56f6e1b1b3e902a2c87bf99b2f40b149817e33
Jacko Dirks <jacko.dirks@gmail.com>
2013-05-15 14:29:15 +0200

    Removes uneseccary spaces

--- main.pl
+++ main.pl
@@ -1,14 +1,22 @@
 #!/usr/bin/perl
-$file = 'example.htm';
-open(MYFILE,$file);
+$inputFile = 'example.htm';
+open(MYFILE,$inputFile);
 @parsedFile=();
 while(<MYFILE>){
-	$_ =~ s/\s+//g;
-	$_ =~ s/(?!>$)>/>\n/g;
+	$_ =~ s/\r\n//g; #strip out all newlines
+	$_ =~ s/(?!>$)>/>\n/g;	#place a newline after each closing tag, except when its the last character on a line
 	@tempArray = split('\n',$_);
 	push(@parsedFile,@tempArray);
-	print("$_\n");
  }
- $arraySize = @parsedFile;
- print "$arraySize\n";
  close (MYFILE); 
+ $arraySize = @parsedFile;
+ @firstIteration=();
+ foreach(@parsedFile) {
+ 	$_ =~ s/^[ \t]+//g; #remove every space on the beginning of a line
+ }
+
+foreach(@parsedFile){
+	print "$_\n"
+}
+
+ print "$arraySize\n";
