83d70bf7f2d7e7921cc9cdf06c3722f47d447a6a
Marcelo Dias Avelino <valorcurse@gmail.com>
2013-05-22 11:07:10 +0200

    yada yada yada

--- main.pl
+++ main.pl
@@ -1,14 +1,40 @@
 #!/usr/bin/perl
 $file = 'example.htm';
-open(MYFILE,$file);
-@parsedFile=();
-while(<MYFILE>){
-	$_ =~ s/\s+//g;
-	$_ =~ s/(?!>$)>/>\n/g;
-	@tempArray = split('\n',$_);
-	push(@parsedFile,@tempArray);
-	print("$_\n");
- }
- $arraySize = @parsedFile;
- print "$arraySize\n";
- close (MYFILE); 
+open(MYFILE, $file);
+# @parsedFile=();
+@data = <MYFILE>;
+$counter = 0;
+$loops = 1;
+
+open (WRITEFILE, '>>data.txt');
+# print WRITEFILE "Bob\n";
+
+foreach (@data){
+	# $_ =~ s/\s+//g;
+	# $_ =~ s/(?!>$)>/>\n/g;
+	# @tempArray = split('\n',$_);
+
+	if ($_ =~ m/<table.*?>/) {
+		$counter++;
+		print "$loops -> ++: $&\n";
+	}
+
+	if ($_ =~ m/<\/table>/) {
+		print "$loops -> --: $&\n";
+		# print WRITEFILE "\n";
+		$counter--;
+	}
+
+	$loops++;
+	# push(@parsedFile,@tempArray);
+	# print("$_\n");
+}
+
+print "$counter\n";
+
+$arraySize = @parsedFile;
+# print "$arraySize\n";
+close (MYFILE); 
+close (WRITEFILE);
+
+# <table.*?>.*<font.*?>.*\n.+\n<\/font>
---
+++ test.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+open ($file, "<" ,'example.htm');
+my $text = join('', <$file>);
+close ($file);
+
+chomp($text);
+
+# Remove break lines
+$text =~ s/\R//g;
+
+my @matches = $text =~ m/<table.*?>.*?(?=<\/font>)<font.*?>.*?<\/font>.*?<\/table>/g;
+
+# print @matches;
+
+# print $text;
+
+# my @matches = $text =~ /(?<=<font size="\d" face="Arial">\r\n).+(?=\r\n<\/font>)/g;
+
+# Remove tabs
+# s{^\s+|\s+$}{}g foreach @matches;
+# s{^<(|\/)b>|\s+$}{}g foreach @matches;
+
+# print @matches
+print "$_\n\n" foreach @matches;
+
+
+# (?<=<font size="\d" face="Arial">\n)(|\t)(|<b>)[\w\(\)\s,.\-:]+(|<\/b>)(?=\n<\/font>)
