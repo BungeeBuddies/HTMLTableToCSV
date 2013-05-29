e92615aba1786d696adbe44235fc33538ba8925f
Jacko Dirks <jacko.dirks@gmail.com>
2013-05-29 10:51:09 +0200

    First conversion to CSV. Some errors remain and one item only shows up on one row (some should show up on multiple rows)

--- main.pl
+++ main.pl
@@ -1,23 +1,25 @@
 #!/usr/bin/perl
-$inputFile = 'example.htm';
-open(MYFILE,$inputFile);
-@parsedFile=();
-while(<MYFILE>){
-	$_ =~ s/\r\n//g; #strip out all newlines
-	$_ =~ s/(?!>$)>/>\n/g;	#place a newline after each closing tag, except when its the last character on a line
-	@tempArray = split('\n',$_);
-	push(@parsedFile,@tempArray);
- }
- close (MYFILE); 
- $arraySize = @parsedFile;
- @firstIteration=();
- foreach(@parsedFile) {
- 	$_ =~ s/^[ \t]+//g; #remove every space on the beginning of a line
- }
+open(FILE, "<" ,'example.htm');
+my $text = join('',<FILE>);
+close (FILE);
+chomp($text);
+$text =~ s/\R//g;
+$text =~ s/<\/b>/ /g;
+$text =~ s/>\s+</></g;
+my @matches;
+push (@matches, $_) foreach $text =~ m/<tr.*?><td.*?><table.*?><tr><td.*?>.+?<\/td><\/tr><\/table><\/td><\/tr>/g;
+for my $match (@matches) {
+	# print "$match\n\n";
 
-foreach(@parsedFile){
-	print "$_\n"
+	my @lineMatches;
+	push (@lineMatches, $_)foreach ($match =~ m/<table.*?><tr><td.*?>(.*?)<\/td><\/tr><\/table>/g);
+	foreach (@lineMatches){
+		$_ =~ s/<\/?(.*?)>//g;		
+		if (++$counter == scalar(@lineMatches)){
+			print "$_";
+		} else {
+			print "$_,";
+		}
+	}
+	print "\n";
 }
-
- print "$arraySize\n";
->>>>>>> ac56f6e1b1b3e902a2c87bf99b2f40b149817e33
